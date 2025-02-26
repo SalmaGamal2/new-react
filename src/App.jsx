ec61a9d86465b922141d7afd622b887badde5922
unknown <salmagmal17@gmail.com>
2025-02-26 23:23:39 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { RiEditBoxLine } from "react-icons/ri";
 import Swal from "sweetalert2";
-import "./APP.CSS";
+import "./App.CSS";
 
 export default function App() {
   const nameInput = useRef();
